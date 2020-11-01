d701a80ae6bfbb6bfb8c4dc2b6c71d7bd7f97bf9
Mateen Mustafa <mateenworld7861@gmail.com>
2020-11-01 01:53:02 -0800

    completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
-import React from "react"
+import React from 'react'
+import { loadStripe } from "@stripe/stripe-js"
+import { Button } from '@material-ui/core'
 
-export default function Home() {
-  return <div>Hello world!</div>
+const stripePromise = loadStripe("pk_test_51HQVBBENGDrlrEfpW5xJCgMyieR7Zl63bKKBLnfuzh7WiZVDp6HZUcezWkCv88EBCQBNDK69yc69XF6T7Tf4a2lt001IT7ZhZb")
+const Index = () => {
+  const redirectToCheckout = async () => {
+    const stripe = await stripePromise
+    const result = await stripe.redirectToCheckout({
+      mode: "payment",
+      lineItems: [{ price: "price_1HiNzaENGDrlrEfpPdV55AKQ", quantity: 2 }],
+      successUrl: "http://localhost:8000/success",
+      cancelUrl: "http://localhost:8000/failed"
+    })
+
+  }
+  return (
+    <div>
+      <Button onClick={redirectToCheckout} color="primary" variant="contained">
+        Checkout
+      </Button>
+    </div>
+  )
 }
+
+export default Index
